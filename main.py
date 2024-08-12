37010b1aa2cb830953ed04bf94ffa0b901ea2d9b
GabrielkSilva <74702968+GabrielkSilva@users.noreply.github.com>
2024-08-12 17:48:28 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import json
 import asyncio
 import aiohttp
 from supabase import create_client, Client
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -16,8 +17,13 @@ with open('items.json', 'r', encoding='utf-8') as f:
     items_data = json.load(f)
 
 cities = ['Bridgewatch', 'Caerleon', 'Fort Sterling', 'Lymhurst', 'Martlock', 'Thetford', 'Black Market']
+blacklist = ['item_to_exclude_1', 'item_to_exclude_2', 'item_to_exclude_3']
 
 async def fetch_item_data(session, item, city):
+    if item['UniqueName'] in blacklist:
+        print(f"Item {item['UniqueName']} está na lista negra e será ignorado.")
+        return None
+
     url = f"https://west.albion-online-data.com/api/v2/stats/prices/{item['UniqueName']}?locations={city}"
     async with session.get(url) as response:
         if response.status == 200:
@@ -28,8 +34,10 @@ async def fetch_item_data(session, item, city):
                     'city': city,
                     'sell_price_min': data[0].get('sell_price_min', 0),
                     'buy_price_max': data[0].get('buy_price_max', 0),
-                    'item_name': item['LocalizedNames']['PT-BR'],
-                    'index': item['Index']
+                    'item_name': item.get('LocalizedNames', {}).get('PT-BR', 'Unknown'),
+                    'index': item['Index'],
+                    'last_updated_date': data[0].get('sell_price_min_date', datetime.utcnow().isoformat()),  # Adicionada vírgula aqui
+                    'last_saved_date': datetime.utcnow().isoformat()
                 }
     return None
 
@@ -41,6 +49,9 @@ async def collect_data(start_index=0):
                 if data:
                     # Inserir ou atualizar dados no Supabase
                     supabase.table('item_prices').upsert(data).execute()
+                    
+                    # Exibir dados resumidos
+                    print(f"Item: {data['item_name']}, City: {data['city']}, Buy Price Max: {data['buy_price_max']}, Sell Price Min: {data['sell_price_min']}, Updated At: {data['last_updated_date']}")
             
             print(f"Processed item: {item['UniqueName']}")
             
@@ -48,9 +59,15 @@ async def collect_data(start_index=0):
             supabase.table('collection_status').update({'current_index': int(item['Index'])}).eq('id', 1).execute()
 
             # Pequena pausa para evitar sobrecarga da API
-            await asyncio.sleep(0.1)
+            await asyncio.sleep(2)
 
-@app.route('/collect.html')
+        # Reiniciar o ciclo se necessário
+        if start_index + len(items_data) >= len(items_data):
+            start_index = 0
+
+
+
+@app.route('/')
 def collect():
     # Obter o índice atual do Supabase
     result = supabase.table('collection_status').select('current_index').execute()
